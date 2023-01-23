69a6bee0728bb623fd5a9d3c3624066e93d7e382
Eroscupid <vutuanduong2003@yahoo.com>
2023-01-23 13:29:15 -0600

    add some change

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,47 @@
 #include "esp_flash.h"
 #include "esp_log.h"
 #include "driver/gpio.h"
+#include <ESP32Servo.h>
+
 #define app_cpu 0
 
+
+Servo myservo; // create servo object to control a servo
+int posVal = 0; // variable to store the servo position
+int servoPin = 15; // Servo motor pin
+
+// motor
+int in1Pin = 12; // Define L293D channel 1 pin
+int in2Pin = 14; // Define L293D channel 2 pin
+int enable1Pin = 27; // Define L293D enable 1 pin
+int channel = 0;
 
 // define task
 static TaskHandle_t task1 = NULL;
 static TaskHandle_t task2 = NULL;
 
+boolean rotationDir; // Define a variable to save the motor's rotation direction
+int rotationSpeed; // Define a variable to save the motor rotation speed
+
+#define PIN_LED 2
+#define PIN_BUTTON 13
+
 const char msg[] = "GE495 capstone project running demo";
 void startTask1 (void *parameter){
-    int msg_len = strlen(msg);
-    while (1){
-      Serial.println();
-      for (int i = 0; i < msg_len;i++){
-        Serial.print(msg[i]);
-
-      }
-      Serial.println();
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println("Button Task has been started");
+  while (1)
+  {
+    if (digitalRead(PIN_BUTTON) == LOW)
+    {
+      Serial.println("button was pressed");
+      digitalWrite(PIN_LED, HIGH);
     }
+    else
+    {
+      digitalWrite(PIN_LED, LOW);
+    }
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
 }
 void startTask2 (void *parameter){
 
@@ -36,12 +58,31 @@ void startTask2 (void *parameter){
     }
 }
 
+void driveMotor(boolean dir, int spd) {
+if (dir) {// Control motor rotation direction
+digitalWrite(in1Pin, HIGH);
+digitalWrite(in2Pin, LOW);
+}
+else {
+digitalWrite(in1Pin, LOW);
+digitalWrite(in2Pin, HIGH);
+}
+ledcWrite(channel, spd); // Control motor rotation speed
+}
+
 void setup() {
   Serial.begin(921600);
   Serial.println("FREE RTOS Start");
-  Serial.print(xPortGetCoreID());
-  Serial.print("with Priority : ");
-  Serial.println(uxTaskPriorityGet(NULL));
+  myservo.setPeriodHertz(50); // standard 50 hz servo
+  myservo.attach(servoPin, 500, 2500); // attaches the servo on servoPin to the servo
+  // Initialize the pin into an output mode:
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+  ledcSetup(channel,1000,11); //Set PWM to 11 bits, range is 0-2047
+  ledcAttachPin(enable1Pin,channel);
+
+
   xTaskCreatePinnedToCore(startTask1,
                           "task 1",
                           1024,
@@ -57,22 +98,38 @@ void setup() {
                           3,
                           &task2,
                           app_cpu );
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT);
 }
 
 void loop() {
   Serial.println("Start");
   vTaskDelay(2000 / portTICK_PERIOD_MS);
-  for (int i = 0; i < 3; i++)
-  {
-      vTaskSuspend(task2);
-      vTaskDelay(2000 / portTICK_PERIOD_MS);
-      vTaskResume(task2);
-      vTaskDelay(2000 / portTICK_PERIOD_MS);
-      }
+  for (posVal = 0; posVal <= 90; posVal += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(posVal); // tell servo to go to position in variable 'pos'  
+  delay(15); // waits 15ms for the servo to reach the position
+  }
+  for (posVal = 90; posVal >= 0; posVal -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(posVal); // tell servo to go to position in variable 'pos'
+    delay(15); // waits 15ms for the servo to reach the position
+  }
 
-      if (task1 !=NULL){
-      vTaskDelete(task1);
-      task1 = NULL;
-      }
-      // test
+  int potenVal = analogRead(A0);// Convert the voltage of rotary potentiometer into digital
+  rotationSpeed = potenVal - 2048;
+  if (potenVal > 2048)
+  rotationDir = true;
+  else
+  rotationDir = false;
+  // Calculate the motor speed
+  rotationSpeed = abs(potenVal - 2048);
+  //Control the steering and speed of the motor
+  driveMotor(rotationDir, constrain(rotationSpeed,0,2048));
 }
+
+
+
+
+
+
+
