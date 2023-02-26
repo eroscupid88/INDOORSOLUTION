4a0c712751c65b1682f301b03fd5cc03f2061285
Eroscupid <vutuanduong2003@yahoo.com>
2023-02-25 21:42:08 -0600

    change

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,14 @@
 #define IN_1_PIN 12 // L293D channel 1 pin
 #define IN_2_PIN 14 // L293D channel 2 pin
 #define EN_1_PIN 13 // L293D enable 1 pin
-#define CHN 0   // PWM channel
 #define FRQ 1000    // PWM frequency
 #define PWM_BIT 11   // PWM Precision
+#define PIN_ANALOG_IN 15  // Potentiometer
+#define PIN_R_PWM_EN 5
+#define PIN_L_PWM_EN 18
+#define CHANNEL 0
+#define PIN_LPWM 19
+#define PIN_RPWM 21
 
 boolean rotationDir; // boolean variable to save the motor's rotation direction
 boolean motorfirstRun; //boolean variable to control motor runtime to 5 seconds
@@ -28,117 +33,108 @@ TaskHandle_t pir_sensor_Handle;
 TaskHandle_t led_strigger_Handle;
 TaskHandle_t dc_motor_Handle;
 
-
-void drive_motor(boolean dir, int spd);
-
-void control_tasks(void *pvParameter){
-
-}
-
-void pir_sensor_task(void *pvParameter) {
-    pinMode(PIR_SENSOR_PIN, INPUT);
-    while (1){
-        Serial.println("Entering PIR SENSOR TASK");
-
-        if (digitalRead(PIR_SENSOR_PIN) == HIGH) {
-            if(xSemaphoreTake(sensorMutex,portMAX_DELAY)==pdTRUE){
-                sensorMode = 1;
-                xSemaphoreGive(sensorMutex);
-            }
-        } else {
-            if(xSemaphoreTake(sensorMutex,portMAX_DELAY)==pdTRUE){
-                sensorMode = 0;
-                xSemaphoreGive(sensorMutex);
-            }
-        }
-        vTaskDelay(100/portTICK_PERIOD_MS);
-        vTaskResume(led_strigger_Handle);
-        vTaskSuspend(NULL);
-    }
-
-}
-
-void dc_motor_task(void *pvParameter){
-    pinMode(IN_1_PIN, OUTPUT);
-    pinMode(IN_2_PIN,OUTPUT);
-    pinMode(EN_1_PIN, OUTPUT);
-    analogWriteFrequency(FRQ);   // USE LEDC(We will for the big motor) to modify PWM precision /analog has precision pwm from 0-255
+// void led_strigger_task(void *pvParmeter){
     
-    // ledcSetup(CHN, FRQ, PWM_BIT);  // Set the PWM frequency to 20 kHz, with 8-bit resolution
-    // ledcAttachPin(EN_1_PIN, CHN);
+//     while(1){
+//         Serial.println("Entering LED TRIGGER");
+//         if(xSemaphoreTake(sensorMutex,portMAX_DELAY)==pdTRUE){
+//             if(sensorMode == 1){
+//                 digitalWrite(LED_PIN,HIGH);
+//             }
+//             else if(sensorMode ==0){
+//                 digitalWrite(LED_PIN,LOW);
+//             }
+//             xSemaphoreGive(sensorMutex);
+//         }
+//         vTaskDelay(100/portTICK_PERIOD_MS);
+//         vTaskResume(dc_motor_Handle);
+//         vTaskSuspend(NULL);
+//     }
 
-    while(1){
-        Serial.println("Entering DC MOTOR TASK");
-        int potenVal = analogRead(A0); //Convert the voltage of rotary potentiometer into digital
-        Serial.println(potenVal);
-        rotationSpeed = potenVal - 2048;
-        if (potenVal >2048) rotationDir = true;
-        else rotationDir = false;
-
-        //Control the motor speed; I divided by 8 because the pwm goes up to 255 but with LEDC you shouldnt
-        rotationSpeed = abs(potenVal - 2048)/8;
-        if (rotationSpeed > 255) rotationSpeed = 255; // same for this one; remove when using ledc
-
-        //Control the steering and speed of the motor
-        
-        if(xSemaphoreTake(sensorMutex,portMAX_DELAY)==pdTRUE){ // test: run the motor if sensor is on
-            if (sensorMode) drive_motor(rotationDir, constrain(rotationSpeed,0,255));
-            else drive_motor(rotationDir,0);
-            xSemaphoreGive(sensorMutex);
-
-
-        }
-        vTaskDelay(100/portTICK_PERIOD_MS);
-
-        vTaskResume(pir_sensor_Handle);
-    }
-
-}
-
-void led_strigger_task(void *pvParmeter){
-    pinMode(LED_PIN, OUTPUT);
-    while(1){
-        Serial.println("Entering LED TRIGGER");
-        if(xSemaphoreTake(sensorMutex,portMAX_DELAY)==pdTRUE){
-            if(sensorMode == 1){
-                digitalWrite(LED_PIN,HIGH);
-                
-            }
-            else if(sensorMode ==0){
-                digitalWrite(LED_PIN,LOW);
-            }
-            xSemaphoreGive(sensorMutex);
-        }
-        vTaskDelay(100/portTICK_PERIOD_MS);
-        vTaskResume(dc_motor_Handle);
-        vTaskSuspend(NULL);
-    }
-
-}
-
+// }
 
 /*Helper Functions*/
 
-void drive_motor(boolean dir, int spd){
-    if(dir){ //Control motor rotation direction
-        digitalWrite(IN_1_PIN,HIGH);
-        digitalWrite(IN_2_PIN, LOW);
-    }
-    else{
-        digitalWrite(IN_1_PIN,LOW);
-        digitalWrite(IN_2_PIN, HIGH);      
-    }
-    analogWrite(EN_1_PIN,spd); // Control motor rotation speed
+// void drive_motor(boolean dir, int spd){
+//     if(dir){ //Control motor rotation direction
+//         digitalWrite(IN_1_PIN,HIGH);
+//         digitalWrite(IN_2_PIN, LOW);
+//     }
+//     else{
+//         digitalWrite(IN_1_PIN,LOW);
+//         digitalWrite(IN_2_PIN, HIGH);      
+//     }
+//     analogWrite(EN_1_PIN,spd); // Control motor rotation speed
 
+// }
+
+void run_motor(int number){
+    Serial.println("motor is running ********");
+    Serial.println(number);
+    ledcWrite(CHANNEL, number);
 }
 
+void pir_sensor_task(void *pvParameter) {
+    
+    while (1)
+    {
+        Serial.println("Entering PIR SENSOR TASK**********************************");
+        if (digitalRead(PIR_SENSOR_PIN) == HIGH) {
+            Serial.println("Sensor Detected>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+//         //     if (xSemaphoreTake(sensorMutex, portMAX_DELAY) == pdTRUE)
+//         //     {
+//         //         sensorMode = 1;
+//         //         xSemaphoreGive(sensorMutex);
+//         //     }
+//         // } else {
+//         //     if(xSemaphoreTake(sensorMutex,portMAX_DELAY)==pdTRUE){
+//         //         sensorMode = 0;
+//         //         xSemaphoreGive(sensorMutex);
+//         //     }
+        }
+        vTaskDelay(100/portTICK_PERIOD_MS);
+//         // vTaskSuspend(NULL);
+    }
+}
+
+void motor_task(void *pvParmeter){
+    Serial.println("Entering Motor Task ");
+    pinMode(PIN_LPWM, OUTPUT);
+    pinMode(PIN_RPWM, OUTPUT);
+    //PWM signal control but Potentiometer
+    ledcSetup(CHANNEL, 1000, 12);
+    ledcAttachPin(PIN_RPWM, CHANNEL);
+    while (1){
+        // Serial.println("Potentiometer Task to set up PWM");
+        // Serial.println("Received Semaphore");
+        // Serial.println(sensorMode);
+        int adcVal = analogRead(PIN_ANALOG_IN); // read adc
+        int pwmVal = adcVal;
+        Serial.println(pwmVal);
+        run_motor(pwmVal);
+            // xSemaphoreGive(sensorMutex);
+        
+        // Timer Activate to suspense the Motor Task
+        // Todo
+
+        // vTaskDelay(1000/portTICK_PERIOD_MS);
+        // vTaskResume(pir_sensor_Handle);
+        
+        
+    }
+}
 
 void setup() {
     Serial.begin(115200);
-    sensorMutex = xSemaphoreCreateMutex();
+    pinMode(PIR_SENSOR_PIN, INPUT);
+    // sensorMutex = xSemaphoreCreateMutex();
     xTaskCreate(pir_sensor_task, "pir_sensor_task", 4096, NULL, 1, &pir_sensor_Handle);
-    xTaskCreate(dc_motor_task, "dc_motor_task", 4096, NULL, 3, &dc_motor_Handle);
-    xTaskCreate(led_strigger_task, "led_trigger_task", 4096, NULL, 2, &led_strigger_Handle);
+    xTaskCreate(motor_task, "motor_task", 4096, NULL, 3, &dc_motor_Handle);
+    
+    // xTaskCreate(dc_motor_task, "dc_motor_task", 4096, NULL, 3, &dc_motor_Handle);
+    // xTaskCreate(motor_task, "motor_task", 4096, NULL, 3,&dc_motor_Handle);
+    // xTaskCreate(led_strigger_task, "led_trigger_task", 4096, NULL, 2, &led_strigger_Handle);
+
     vTaskStartScheduler();
 }
 
