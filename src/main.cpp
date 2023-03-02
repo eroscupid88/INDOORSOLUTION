4726c68572420529fbc1cdb019b5a139e2da99cb
Eroscupid <vutuanduong2003@yahoo.com>
2023-03-02 14:00:30 -0600

    change main

--- src/main.cpp
+++ src/main.cpp
@@ -10,60 +10,158 @@
 #include "driver/gpio.h"
 #include <ESP32Servo.h>
 
-#define app_cpu 0
-// define task
-static TaskHandle_t task1 = NULL;
-static TaskHandle_t task2 = NULL;
 
-#define PIN_LED 2
-#define PIN_BUTTON 13
+// Use only core 1 for demo purposes
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
 
-const char msg[] = "GE495 capstone project running demo";
-void startTask1 (void *parameter){
-  while (1)
-  {
-    Serial.println("Task1");
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
+// Settings
+TickType_t cs_wait = 250;  // Time spent in critical section (ms)
+TickType_t med_wait = 5000; // Time medium task spends working (ms)
+
+// Globals
+static SemaphoreHandle_t lock;
+
+//*****************************************************************************
+// Tasks
+
+// Task L (low priority)
+void doTaskL(void *parameters) {
+
+  TickType_t timestamp;
+
+  // Do forever
+  while (1) {
+
+    // Take lock
+    Serial.println("Task L trying to take lock...");
+    timestamp = xTaskGetTickCount() * portTICK_PERIOD_MS;
+    xSemaphoreTake(lock, portMAX_DELAY);
+
+    // Say how long we spend waiting for a lock
+    Serial.print("Task L got lock. Spent ");
+    Serial.print((xTaskGetTickCount() * portTICK_PERIOD_MS) - timestamp);
+    Serial.println(" ms waiting for lock. Doing some work...");
+
+    // Hog the processor for a while doing nothing
+    timestamp = xTaskGetTickCount() * portTICK_PERIOD_MS;
+    while ( (xTaskGetTickCount() * portTICK_PERIOD_MS) - timestamp < cs_wait);
+
+    // Release lock
+    Serial.println("Task L releasing lock.");
+    xSemaphoreGive(lock);
+
+    // Go to sleep
+    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
-void startTask2 (void *parameter){
 
-    while (1){
-      Serial.println("Task2");
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+// Task M (medium priority)
+void doTaskM(void *parameters) {
+
+  TickType_t timestamp;
+
+  // Do forever
+  while (1) {
+
+    // Hog the processor for a while doing nothing
+    Serial.println("Task M doing some work...");
+    timestamp = xTaskGetTickCount() * portTICK_PERIOD_MS;
+    while ( (xTaskGetTickCount() * portTICK_PERIOD_MS) - timestamp < med_wait);
+
+    // Go to sleep
+    Serial.println("Task M done!");
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
 }
 
+// Task H (high priority)
+void doTaskH(void *parameters) {
+
+  TickType_t timestamp;
+
+  // Do forever
+  while (1) {
+
+    // Take lock
+    Serial.println("Task H trying to take lock...");
+    timestamp = xTaskGetTickCount() * portTICK_PERIOD_MS;
+    xSemaphoreTake(lock, portMAX_DELAY);
+
+    // Say how long we spend waiting for a lock
+    Serial.print("Task H got lock. Spent ");
+    Serial.print((xTaskGetTickCount() * portTICK_PERIOD_MS) - timestamp);
+    Serial.println(" ms waiting for lock. Doing some work...");
+
+    // Hog the processor for a while doing nothing
+    timestamp = xTaskGetTickCount() * portTICK_PERIOD_MS;
+    while ( (xTaskGetTickCount() * portTICK_PERIOD_MS) - timestamp < cs_wait);
+
+    // Release lock
+    Serial.println("Task H releasing lock.");
+    xSemaphoreGive(lock);
+    
+    // Go to sleep
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+//*****************************************************************************
+// Main (runs as its own task with priority 1 on core 1)
 
 void setup() {
-  Serial.begin(921600);
-  Serial.println("FREE RTOS Start");
 
-  xTaskCreatePinnedToCore(startTask1,
-                          "task 1",
+  // Configure Serial
+  Serial.begin(921600);
+
+  // Wait a moment to start (so we don't miss Serial output)
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("---FreeRTOS Priority Inheritance Demo---");
+
+  // Create semaphores and mutexes before starting tasks
+  lock = xSemaphoreCreateMutex();
+  
+  // The order of starting the tasks matters to force priority inversion
+
+  // Start Task L (low priority)
+  xTaskCreatePinnedToCore(doTaskL,
+                          "Task L",
                           1024,
                           NULL,
-                          2,
-                          &task1,
-                          app_cpu );
+                          1,
+                          NULL,
+                          app_cpu);
 
-  xTaskCreatePinnedToCore(startTask2,
-                          "task 2",
+  // Introduce a delay to force priority inversion
+  vTaskDelay(1 / portTICK_PERIOD_MS);
+
+  // Start Task H (high priority)
+  xTaskCreatePinnedToCore(doTaskH,
+                          "Task H",
                           1024,
                           NULL,
                           3,
-                          &task2,
-                          app_cpu );
-  pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_BUTTON, INPUT);
+                          NULL,
+                          app_cpu);
+
+  // Start Task M (medium priority)
+  xTaskCreatePinnedToCore(doTaskM,
+                          "Task M",
+                          1024,
+                          NULL,
+                          2,
+                          NULL,
+                          app_cpu);
+
+  // Delete "setup and loop" task
+  vTaskDelete(NULL);
 }
 
 void loop() {
+  // Execution should never get here
 }
-
-
-
-
-
-
 
