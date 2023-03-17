2865be104889331c83195a45de1479ea75b9af3c
Eroscupid <vutuanduong2003@yahoo.com>
2023-03-17 16:19:50 -0600

    stepper motor workgit status

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "esp_timer.h"
-#include <Stepper.h>
 
 
 // Use only core 1 for demo purposes
@@ -49,216 +48,89 @@ TaskHandle_t reset_button_Handle;
 #define PIN_ANALOG_IN 15  // Potentiometer
 #define CHANNEL 0
 #define BUTTON_PRESS_DELAY_MS 2000 //Length of button press
-
-
+#define REVERSE_SWITCH 14
+#define DRIVER_PUL 18
+#define DRIVER_DIR 19
+#define stepsPerRevolution 800
+int pd = 500;
 
 //************************************************************* Globals ****************************************
-static SemaphoreHandle_t lock;
-// AccelStepper myStepper(AccelStepper::FULL4WIRE, In1, In3, In2, In4); 
-
-int sensorMode = 0;
-int doorMode = 0;
-
-
 // Defin pins
- 
-int reverseSwitch = 14;  // Push button for reverse
-int driverPUL = 18;    // PUL- pin 
-int driverDIR = 19;    // DIR- pin
-int spd = 15;     // Potentiometer
-
-// Variables
-int pd = 500;       // Pulse Delay period
-boolean setdir = LOW; // Set Direction
 
 //******************************************** Help Functions ****************************************
 
-void initialize_timer(){
-  //     TIMER 0
-  esp_timer_create_args_t timer0_args = {.callback = timer0_callback,.name = "timer0"};
-  esp_timer_create(&timer0_args, &timer0_handle);
+
+void run_motor(){
   
-  //     TIMER 1 
-  esp_timer_create_args_t timer1_args = {.callback = timer1_callback,.name = "timer1"};
-  esp_timer_create(&timer1_args, &timer1_handle);
 }
 
-/************************************TIMERS************************************************************/
-// Callback function for TIMER0 when Door is in wide open 
-void timer0_callback(void* arg) {
-    Serial.println("                                        TIMER0 triggered  8s  ");
-    esp_timer_start_once(timer1_handle, 4000000);  // 4 seconds
-    doorMode = 2;
-}
-// Callback function for TIMER1 when Door is release (auto closer work now)
-void timer1_callback(void* arg) {
-    Serial.println("                                        TIMER1 triggered  4s  ");
-    doorMode = 3;
+void drive_the_motor_to_open_door(){
+  int adcVal = analogRead(PIN_ANALOG_IN); // read adc
+  int pwmVal = adcVal;
+  Serial.println(pwmVal);
+  // run_motor(pwmVal);
 }
 
 //************************************* Tasks ************************************************
 
-void resetButtonTask(void *pvParameters)
-{
-  pinMode(RESET_BUTTON_PIN, INPUT_PULLUP); // Set the reset button pin as input with pull-up resistor
-  
-  bool isTaskSuspended = true; // Initialize a flag to keep track of the task's state
-
-  while (1)
-  {
-    if (digitalRead(RESET_BUTTON_PIN) == LOW) // If the reset button is pressed
-    {
-      if (isTaskSuspended) // If the task is currently suspended, resume it
-      {
-        xTaskResumeFromISR(dc_motor_Handle); // Resume the task from ISR
-        xTaskResumeFromISR(pir_sensor_Handle); // Resume the task from ISR
-        doorMode = 0;
-        isTaskSuspended = false; // Update the task state flag
-        Serial.println("                              Resume the MOTOR TASK");
-      }
-      else // If the task is currently running, suspend it
-      {
-        vTaskSuspend(dc_motor_Handle); // Suspend the task
-        vTaskSuspend(pir_sensor_Handle);
-        doorMode = 3;
-        esp_timer_stop(timer0_handle);
-        esp_timer_stop(timer1_handle);
-        isTaskSuspended = true; // Update the task state flag
-        Serial.println("                              Suspense the MOTOR TASK");
-      }
-      delay(100); // Wait for 1 s to debounce the button
-    }
-    else
-    {
-      delay(50); // Wait for 0.5s before checking the button state again
-    }
-  }
-}
-
-void run_motor(){
-  Serial.print("Motor is running");
-  pd = map((analogRead(spd)), 0, 1023, 2000, 50);
-  digitalWrite(driverDIR,setdir);
-  digitalWrite(driverPUL,HIGH);
-  delayMicroseconds(pd);
-  digitalWrite(driverPUL,LOW);
-  delayMicroseconds(pd);
-}
-
-
-
-// Task L (low priority)
-void doTaskL(void *parameters) {
-
-  TickType_t timestamp;
-
-  // Do forever
-  while (1) {
-    xSemaphoreTake(lock, portMAX_DELAY);
-    // Say how long we spend waiting for a lock
-    Serial.print("Task Sensor got lock. Spent ");
-    // Hog the processor for a while doing nothing
-    timestamp = xTaskGetTickCount() * portTICK_PERIOD_MS;
-    // while ( (xTaskGetTickCount() * portTICK_PERIOD_MS) - timestamp < cs_wait);
-
-    // do something over here
-    if (digitalRead(PIR_SENSOR_PIN) == HIGH && doorMode == 0){
-      sensorMode = 1;
-      doorMode = 1;
-    }
-    else if (digitalRead(PIR_SENSOR_PIN) == HIGH && doorMode == 2){
-      // reset timer 1 if sensor is activate when door is on hold
-      sensorMode = 1;
-      Serial.print("                                          RESET timer 1");
-      esp_timer_stop(timer1_handle);
-      esp_timer_start_once(timer1_handle, 4000000); // 4 seconds
-    }
-    else {
-      sensorMode = 0;
-    }
-    xSemaphoreGive(lock);
-
-    // Go to sleep
-    vTaskDelay(250 / portTICK_PERIOD_MS);
-  }
-}
-
-// Task M (medium priority)
-void doTaskM(void *parameters) {
-  while (1) {
-    
-    if(sensorMode == 1){
-      // Serial.print("Task LED detect a sensor >>>>>>> turn the LED on");
-      digitalWrite(LED_PIN,HIGH);
-    }
-    else if(sensorMode ==0){
-      digitalWrite(LED_PIN,LOW);
-    }
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-  }
-}
-
 // Task H (high priority)
 void doTaskH(void *parameters) {
   while (1) {
-    // Serial.println("Motor Task DOING SOME WORK...");
-    // xSemaphoreTake(lock, portMAX_DELAY);
-    // MOTOR DOING SOME WORK HERE
-    run_motor();
-    // xSemaphoreGive(lock);
+    // Set the spinning direction clockwise:
+  digitalWrite(DRIVER_DIR, HIGH);
 
-    // Go to sleep
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+  // Spin the stepper motor 1 revolution slowly:
+  for (int i = 0; i < 50*stepsPerRevolution; i++) {
+    // These four lines result in 1 step:
+    digitalWrite(DRIVER_PUL, HIGH);
+    delayMicroseconds(150);
+    digitalWrite(DRIVER_PUL, LOW);
+    delayMicroseconds(150);    
+  }
+  delay(1000);
+
+  // digitalWrite(DRIVER_DIR, LOW);
+
+  // // Spin the stepper motor 1 revolution slowly:
+  // for (int i = 0; i < 10*stepsPerRevolution; i++) {
+  //   // These four lines result in 1 step:
+  //   digitalWrite(DRIVER_PUL, HIGH);
+  //   delayMicroseconds(pd);
+  //   digitalWrite(DRIVER_PUL, LOW);
+  //   delayMicroseconds(pd);
+  // }
+  // delay(1000);
+
+
+
   }
 }
 
-void revmotor (){
-  Serial.print("#################                 change dirrection");
-  setdir = !setdir;
-}
 
 
 //*****************************************************************************
 // Main (runs as its own task with priority 1 on core 1)
-
 void setup() {
   // Configure Serial
   Serial.begin(921600);
-
-  initialize_timer();
   // GPIO PIN
-  pinMode(PIR_SENSOR_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
   // pinMode(RESET_BUTTON_PIN, INPUT_PULLUP);
-  ledcSetup(CHANNEL, 1000, 12);
-
-  pinMode (driverPUL, OUTPUT);
-  pinMode (driverDIR, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(reverseSwitch), revmotor, FALLING);
-
   // need to fix this
-  // ledcAttachPin(PIN_RPWM, CHANNEL);
+  // set the maximum speed and initial speed.
+  
+  // set up stepper motor
+  pinMode(DRIVER_DIR, OUTPUT);
+  pinMode(DRIVER_PUL, OUTPUT);
       // Create the fire alarm timer   
     
   // Wait a moment to start (so we don't miss Serial output)
   vTaskDelay(1000 / portTICK_PERIOD_MS);
 
   // Create semaphores and mutexes before starting tasks
-  lock = xSemaphoreCreateMutex();
   
   // The order of starting the tasks matters to force priority inversion
 
   
-  // Start Button Task (low priority)
-  // xTaskCreatePinnedToCore(resetButtonTask, "resetButtonTask", 10000, NULL, configMAX_PRIORITIES - 1, NULL, app_cpu); // Create the reset button task with the highest priority
-  // Start Sensor Task (low priority)
-  xTaskCreatePinnedToCore(doTaskL,
-                          "Task L",
-                          1024,
-                          NULL,
-                          4,
-                          &pir_sensor_Handle,
-                          app_cpu);
 
   // Introduce a delay to force priority inversion
   vTaskDelay(1 / portTICK_PERIOD_MS);
@@ -271,16 +143,6 @@ void setup() {
                           3,
                           &dc_motor_Handle,
                           app_cpu);
-
-  // Start LED Task (medium priority)
-  xTaskCreatePinnedToCore(doTaskM,
-                          "Task M",
-                          1024,
-                          NULL,
-                          2,
-                          &led_strigger_Handle,
-                          app_cpu);
-
 
   // Delete "setup and loop" task
   vTaskDelete(NULL);
